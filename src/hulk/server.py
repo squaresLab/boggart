f930362e5cf1da09d50e4055aff2f53fc44d940d
ChrisTimperley <christimperley@googlemail.com>
2017-12-08 23:41:09 -0500

    added basic error handling to server daemon

--- src/hulk/server.py
+++ src/hulk/server.py
@@ -1,7 +1,11 @@
+import flask
 from flask import Flask
 
-
 app = Flask(__name__)
+
+def json_error(msg):
+    jsn = {'error': {'msg': msg}}
+    return flask.jsonify(jsn)
 
 
 @app.route('/')
@@ -11,7 +15,19 @@ def hello_world():
 
 @app.route('/mutations', methods=['GET'])
 def mutations():
-    return "Here's a list of mutations..."
+    try:
+        args = flask.request.get_json()
+
+        if 'filepath' not in args:
+            return json_error("No 'filepath' argument provided.")
+        filepath = args['filepath']
+
+        mutations = []
+
+        return flask.jsonify(mutations)
+
+    except JSONException as e:
+        return e.jsonify()
 
 
 def launch(port: int = 6000) -> None:
