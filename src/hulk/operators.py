f2871757a723cb42609a15d7dbec06206700c201
ChrisTimperley <christimperley@googlemail.com>
2017-12-09 00:47:35 -0500

    added operators module

---
+++ src/hulk/operators.py
@@ -0,0 +1,40 @@
+from exceptions import *
+from typing import List, Tuple, Dict
+from base import Operator, Transformation, Language
+
+# A registry of operators, stored by their names
+__OPERATORS : Dict[str, Operator] = {}
+
+
+# TODO: Load from YAML files!
+def register(name: str,
+             languages: List[Language],
+             transformations: List[Tuple[str, str]]) -> None:
+    """
+    Constructs and registers a mutation operator with Hulk.
+
+    Params:
+        name: The unique name of the mutation operator.
+        languages: A list of languages supported by the mutation operator.
+        transformations: A list of transformations that implement this
+            mutation operator, given as plaintext Rooibos (match, rewrite)
+            pairs.
+
+    Raises:
+        OperatorNameAlreadyExists: if another operator is already registered
+            under the provided name.
+    """
+    if name in __OPERATORS:
+        raise OperatorNameAlreadyExists(name)
+
+    transformations = \
+        [Transformation(match, rewrite) for (match, rewrite) in transformations]
+    op = Operator(name, languages, transformations)
+    __OPERATORS[name] = op
+
+    return op
+
+
+register("NEGATE_IF_CONDITION-CSTYLE",
+         [Language.C, Language.CXX, Language.JAVA],
+         [('if (:[1])', 'if (!(:[1]))')])
