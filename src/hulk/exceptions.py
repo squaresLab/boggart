9bd061f810ab7dea50412ecf2b3afcffeaa6d981
ChrisTimperley <christimperley@googlemail.com>
2017-12-09 01:49:49 -0500

    added 'LanguageNotFound' exception

--- src/hulk/exceptions.py
+++ src/hulk/exceptions.py
@@ -3,11 +3,29 @@ class OperatorNameAlreadyExists(Exception):
     Used to indicate that a given operator name is already in use by another
     operator.
     """
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
         self.__name = name
         msg = "operator name is already in use: {}".format(name)
         super().__init__(msg)
 
     @property
     def name(self) -> str:
+        return self.__name
+
+
+class LanguageNotFound(Exception):
+    """
+    Used to indicate that there exists no language registered under a given
+    name.
+    """
+    def __init__(self, name: str) -> None:
+        self.__name = name
+        msg = "no language registered with name: {}".format(name)
+        super().__init__(msg)
+
+    @property
+    def name(self) -> str:
+        """
+        The name of the requested language.
+        """
         return self.__name
