9a8a52e16b1573fde24f08a59094feb9a8a1a133
ChrisTimperley <christimperley@googlemail.com>
2017-12-09 13:54:57 -0500

    tweaked setup to include package data

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
         'requests',
         'flask'
     ],
+    include_package_data=True,
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
