fc90074053fab0d4adf20140067071d3f9b8a89c
ChrisTimperley <christimperley@googlemail.com>
2017-12-09 00:22:31 -0500

    added basic mutation operator class

--- src/hulk/base.py
+++ src/hulk/base.py
@@ -47,3 +47,33 @@ class Language(Enum):
         information is not explicitly provided.
         """
         return self.__file_endings.__iter__()
+
+
+class Operator(object):
+    """
+    Used to describe a mutation operator. Each mutation operator has its own
+    own unique name that is used by external interfaces (i.e., the server) to
+    lookup particular operators.
+    """
+    def __init__(self,
+                 name: str,
+                 languages: List[Language]) -> None:
+        assert name != '', "operator must have a non-empty name"
+        assert languages != [], "operator must support at least one language"
+
+        self.__name = name
+        self.__languages = frozenset(languages)
+
+    @property
+    def name(self) -> str:
+        """
+        The name of this mutation operator.
+        """
+        return self.__name
+
+    @property
+    def languages(self) -> Iter[Language]:
+        """
+        The languages supported by this mutation operator.
+        """
+        return self.__languages.__iter__()
