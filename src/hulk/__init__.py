b2f51eb91ceb60191b5f47d1a66adedfba06a1f1
ChrisTimperley <christimperley@googlemail.com>
2017-12-10 00:12:21 -0500

    removed old 'operators' module

--- src/hulk/__init__.py
+++ src/hulk/__init__.py
@@ -1,7 +1,7 @@
 import hulk.base
 import hulk.hulk
-import hulk.operators
 import hulk.exceptions
 import hulk.server
 
 from hulk.base import Operator, Transformation, Language
+from hulk.hulk import Hulk
