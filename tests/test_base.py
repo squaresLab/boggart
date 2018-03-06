b94b3ff20b20ba0c9e652765ce8b25863996393f
ChrisTimperley <christimperley@googlemail.com>
2018-03-06 15:57:37 -0500

    added test_language_equivalence

--- tests/test_base.py
+++ tests/test_base.py
@@ -6,3 +6,14 @@ def test_serialisation():
     expected = Language("c", [".c"])
     actual = Language.from_dict(expected.to_dict())
     assert actual == expected
+
+def test_language_equivalence():
+    a = Language("java", [".java"])
+    b = Language("java", [".java"])
+    c = Language("java", [".foo"])
+    d = Language("notjava", [".java"])
+
+    assert a == b
+    assert b == a
+    assert a != c
+    assert a != d
