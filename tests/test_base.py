c95503294da31b8b5c4c04c43853a7da16f29ace
ChrisTimperley <christimperley@googlemail.com>
2018-03-06 17:43:29 -0500

    added 'test_transformation_serialisation'

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,11 +1,18 @@
 import pytest
-from hulk.base import Language
+from hulk.base import Language, Transformation, Operator
 
 
-def test_serialisation():
+def test_transformation_serialisation():
+    expected = Transformation(':x = :y', ':y = :x')
+    actual = Transformation.from_dict(expected.to_dict())
+    assert expected == actual
+
+
+def test_language_serialisation():
     expected = Language("c", [".c"])
     actual = Language.from_dict(expected.to_dict())
     assert actual == expected
+
 
 def test_language_equivalence():
     a = Language("java", [".java"])
