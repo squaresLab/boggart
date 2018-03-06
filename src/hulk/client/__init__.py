079a0b9687294e68ff3c836c5d517a78d6166942
ChrisTimperley <christimperley@googlemail.com>
2018-03-06 11:03:07 -0500

    added _url and _get to client

--- src/hulk/client/__init__.py
+++ src/hulk/client/__init__.py
@@ -1,3 +1,8 @@
+import urlparse
+import requests
+from .languages import LanguageCollection
+
+
 class Client(object):
     """
     A client for communicating with a Hulk server.
@@ -15,11 +20,28 @@ class Client(object):
         self.__base_url = base_url
         self.__timeout = timeout
 
-
     @property
-    def languages() -> LanguageCollection:
+    def languages(self) -> LanguageCollection:
         """
         The set of languages that are supported (i.e., can be mutated)
         by the server.
         """
+        # TODO: cache?
         return LanguageCollection(client=self)
+
+    def _url(self, path: str) -> str:
+        """
+        Computes the URL to a resource located at a given path on the server.
+        """
+        url = urlparse.urljoin(self.__base_url, path)
+        return str(url)
+
+    def _get(self, path: str, params = None, **kwargs) -> requests.Response:
+        """
+        Sends a GET request to the server.
+
+        Parameters:
+            path:   the path of the resource.
+        """
+        url = self._url(path)
+        return requests.get(url, params, kwargs)
