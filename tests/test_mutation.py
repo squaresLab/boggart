ba96228890be88178ccee68921b19efe0cac5f79
ChrisTimperley <christimperley@googlemail.com>
2018-04-19 12:15:37 -0400

    updated tests

--- tests/test_mutation.py
+++ tests/test_mutation.py
@@ -10,11 +10,11 @@ def test_eq():
     op = 'foo'
     args = {'x': 'bar'}
 
-    m = Mutation(op, lx, args)
-    m_identical = Mutation(op, lx, args)
-    m_different_operator = Mutation('bar', lx, args)
-    m_different_location = Mutation(op, ly, args)
-    m_different_args = Mutation(op, lx, {'y': 'hello'})
+    m = Mutation(op, 0, lx, args)
+    m_identical = Mutation(op, 0, lx, args)
+    m_different_operator = Mutation('bar', 0, lx, args)
+    m_different_location = Mutation(op, 0, ly, args)
+    m_different_args = Mutation(op, 0, lx, {'y': 'hello'})
 
     assert m != None
     assert m == m_identical
@@ -26,10 +26,11 @@ def test_eq():
 def test_to_and_from_dict():
     location = LocationRange(Location(1, 5),
                              Location(2, 30))
-    mutation = Mutation('foo', location, {'x': '120'})
+    mutation = Mutation('foo', 0, location, {'x': '120'})
 
     expected = {
         'operator': 'foo',
+        'transformation-index': 0,
         'location': '1:5::2:30',
         'arguments': {'x': '120'}
     }
