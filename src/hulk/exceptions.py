22fc4229f16662dfd1560aab9e30f2f91c8bd9bf
ChrisTimperley <christimperley@googlemail.com>
2018-04-19 14:08:14 -0400

    added separate notion of server errors

--- src/hulk/exceptions.py
+++ src/hulk/exceptions.py
@@ -1,5 +1,8 @@
+import flask
+
 __ALL__ = [
     'HulkException',
+    'ServerError',
     'OperatorNameAlreadyExists',
     'LanguageNotFound',
     'BadConfigFile',
@@ -11,14 +14,45 @@ class HulkException(Exception):
     """
     Base class for all exceptions produced by Hulk.
     """
-    pass
+
+class ServerError(HulkException):
+    """
+    Base class for all exceptions that may be thrown from the server to the
+    client. Provides the necessary machinery to write/read errors to/from
+    JSON descriptions.
+    """
+    @staticmethod
+    def from_dict(d: dict) -> 'HulkException':
+        assert 'kind' in d
+
+        # TODO use metaprogramming to avoid having to maintain this list
+        cls = ({
+            'OperatorNameAlreadyExists': OperatorNameAlreadyExists,
+            'LanguageNotFound': LanguageNotFound,
+            'BadConfigFile': BadConfigFile,
+            'IllegalConfig': IllegalConfig
+        })[d]
+
+        return cls.from_dict(d)
+
+    def to_response(self) -> flask.Response:
+        """
+        Transforms this exception into a HTTP response containing a
+        machine-readable description of the exception.
+        """
+        raise NotImplementedError
 
 
-class OperatorNameAlreadyExists(HulkException):
+class OperatorNameAlreadyExists(ServerError):
     """
     Used to indicate that a given operator name is already in use by another
     operator.
     """
+    @staticmethod
+    def from_dict(d: dict) -> 'OperatorNameAlreadyExists':
+        assert 'name' in d
+        return OperatorNameAlreadyExists(d['name'])
+
     def __init__(self, name: str) -> None:
         self.__name = name
         msg = "operator name is already in use: {}".format(name)
@@ -32,11 +66,16 @@ class OperatorNameAlreadyExists(HulkException):
         return self.__name
 
 
-class LanguageNotFound(HulkException):
+class LanguageNotFound(ServerError):
     """
     Used to indicate that there exists no language registered under a given
     name.
     """
+    @staticmethod
+    def from_dict(d: dict) -> 'LanguageNotFound':
+        assert 'name' in d
+        return LanguageNotFound(d['name'])
+
     def __init__(self, name: str) -> None:
         self.__name = name
         msg = "no language registered with name: {}".format(name)
@@ -50,7 +89,7 @@ class LanguageNotFound(HulkException):
         return self.__name
 
 
-class BadConfigFile(HulkException):
+class BadConfigFile(ServerError):
     """
     Used to indicate that a given configuration file is ill-formed.
     """
@@ -58,7 +97,7 @@ class BadConfigFile(HulkException):
         super().__init__(reason)
 
 
-class IllegalConfig(HulkException):
+class IllegalConfig(ServerError):
     """
     Used to indicate that a given configuration is syntatically correct but
     that it describes an illegal configuration.
