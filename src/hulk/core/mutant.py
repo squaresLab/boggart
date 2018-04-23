48ec4149d003e08fb220fa2feeaaf79ca8926d3d
ChrisTimperley <christimperley@googlemail.com>
2018-04-23 16:24:09 -0400

    added mutations property to Mutant

--- src/hulk/core/mutant.py
+++ src/hulk/core/mutant.py
@@ -1,3 +1,5 @@
+from typing import List, Iterator
+
 from .mutation import Mutation
 
 __all__ = ['Mutant']
@@ -5,9 +7,20 @@ __all__ = ['Mutant']
 
 class Mutant(object):
     def __init__(self,
-                 snapshot: str
+                 snapshot: str,
+                 mutations: List[Mutation]
                  ) -> None:
+        """
+        Constructs a new Mutant description.
+
+        Parameters:
+            snapshot: the name of the snapshot that was used to generate the
+                mutant.
+            mutations: the sequence of mutations that were applied to the
+                snapshot.
+        """
         self.__snapshot = snapshot
+        self.__mutations = mutations
 
     @property
     def snapshot(self) -> str:
@@ -15,3 +28,11 @@ class Mutant(object):
         The name of the snapshot that was used to generate this mutant.
         """
         return self.__snapshot
+
+    @property
+    def mutations(self) -> Iterator[Mutation]:
+        """
+        Returns an iterator over the mutations that were applied to the
+        snapshot in order to generate this mutant.
+        """
+        return self.__mutations.__iter__()
