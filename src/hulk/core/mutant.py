3d93ffae4b31d6df5ecd7f4decc72765f915e3e7
ChrisTimperley <christimperley@googlemail.com>
2018-04-23 16:22:16 -0400

    added snapshot property to mutant

--- src/hulk/core/mutant.py
+++ src/hulk/core/mutant.py
@@ -4,4 +4,14 @@ __all__ = ['Mutant']
 
 
 class Mutant(object):
-    pass
+    def __init__(self,
+                 snapshot: str
+                 ) -> None:
+        self.__snapshot = snapshot
+
+    @property
+    def snapshot(self) -> str:
+        """
+        The name of the snapshot that was used to generate this mutant.
+        """
+        return self.__snapshot
