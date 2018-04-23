390d67dccaa9f99e9f4414465f133f89199eb570
ChrisTimperley <christimperley@googlemail.com>
2018-04-23 19:51:21 -0400

    added read_chars to source manager and added stub line_col_to_offset

--- src/hulk/server/sourcefile.py
+++ src/hulk/server/sourcefile.py
@@ -3,6 +3,7 @@ from typing import Dict, Tuple
 from bugzoo.core.bug import Bug
 from bugzoo.client import Client as BugZooClient
 
+from ..core import FileLocationRange
 from ..exceptions import *
 
 __all__ = ['SourceFileManager']
@@ -12,6 +13,17 @@ class SourceFileManager(object):
     def __init__(self, client_bugzoo: BugZooClient) -> None:
         self.__cache_contents_cache = {} # type: Dict[Tuple[str, str], str]
 
+    def line_col_to_offset(self,
+                           snapshot: Bug,
+                           filepath: str,
+                           line_num: int,
+                           col_num: int) -> int:
+        """
+        Transforms a line-column number for a given file belonging to a
+        BugZoo snapshot into a zero-indexed character offset.
+        """
+        raise NotImplementedError
+
     def read_file(self, snapshot: Bug, filepath: str) -> str:
         """
         Fetches the contents of a specified source code file belonging to a
@@ -20,6 +32,8 @@ class SourceFileManager(object):
         Raises:
             FileNotFound: if the given file is not found inside the snapshot.
         """
+        # TODO normalise file path
+
         bgz = self.__bugzoo
         key_cache = (snapshot.name, filepath)
         if key_cache in self.__cache_file_contents:
@@ -35,3 +49,25 @@ class SourceFileManager(object):
 
         self.__cache_file_contents[key_cache] = contents
         return contents
+
+    def read_chars(self, snapshot: Bug, location: FileLocationRange) -> str:
+        """
+        Fetches a specified sequence of characters from a source code file
+        belonging to a BugZoo snapshot.
+
+        Raises:
+            FileNotFound: if the given file is not found inside the snapshot.
+        """
+        filename = location.filepath
+        contents_file = self.read_file(filename)
+
+        start_at = self.line_col_to_offset(snapshot,
+                                           filename,
+                                           location.start.line_num,
+                                           location.start_col_num)
+        stop_at = self.line_col_to_offset(snapshot,
+                                           filename,
+                                           location.stop.line_num,
+                                           location.stop.col_num)
+
+        return contents_file[start_at:stop_at + 1]
