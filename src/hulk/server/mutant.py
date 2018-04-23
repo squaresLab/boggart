6cccbd4cedd9eaa5e9ca715c0b01cb4460fbffa5
ChrisTimperley <christimperley@googlemail.com>
2018-04-23 16:42:21 -0400

    added __iter__, __getitem__, and __len__ to mutant manager

--- src/hulk/server/mutant.py
+++ src/hulk/server/mutant.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import List, Iterator, Dict
+from uuid import UUID
 
 from bugzoo.client import Client as BugZooClient
 
@@ -9,5 +10,27 @@ class MutantManager(object):
     def __init__(self,
                  client_bugzoo: BugZooClient
                  ) -> None:
-        self.__mutants = [] # type: List[Mutant]
+        self.__mutants = [] # type: Dict[UUID, Mutant]
         self.__bugzoo = client_bugzoo
+
+    def __iter__(self) -> Iterator[UUID]:
+        """
+        Returns an iterator over the UUIDs of the mutants that are currently
+        registered with this server.
+        """
+        yield from self.__mutants.keys()
+
+    def __getitem__(self, uuid: UUID) -> Mutant:
+        """
+        Retrieves a registered mutant by its UUID.
+
+        Raises:
+            KeyError: if no mutant is registered under the given UUID.
+        """
+        return self.__mutants[uuid]
+
+    def __len__(self) -> int:
+        """
+        Returns a count of the number of registered mutants.
+        """
+        return len(self.__mutants)
