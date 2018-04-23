9a375a92b3e302b9c1b2eabe646fe4172f6a0732
ChrisTimperley <christimperley@googlemail.com>
2018-04-23 16:25:56 -0400

    added uuid to Mutant

--- src/hulk/core/mutant.py
+++ src/hulk/core/mutant.py
@@ -1,4 +1,5 @@
 from typing import List, Iterator
+from uuid import UUID
 
 from .mutation import Mutation
 
@@ -7,6 +8,7 @@ __all__ = ['Mutant']
 
 class Mutant(object):
     def __init__(self,
+                 uuid: UUID,
                  snapshot: str,
                  mutations: List[Mutation]
                  ) -> None:
@@ -14,13 +16,22 @@ class Mutant(object):
         Constructs a new Mutant description.
 
         Parameters:
+            uuid: the UUID for the mutant.
             snapshot: the name of the snapshot that was used to generate the
                 mutant.
             mutations: the sequence of mutations that were applied to the
                 snapshot.
         """
+        self.__uuid = uuid
         self.__snapshot = snapshot
         self.__mutations = mutations
+
+    @property
+    def uuid(self) -> UUID:
+        """
+        The UUID for this mutant.
+        """
+        return self.__uuid
 
     @property
     def snapshot(self) -> str:
