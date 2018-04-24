2ec6949c1bd429dc76d665e027ef7e26ec0407e8
ChrisTimperley <christimperley@googlemail.com>
2018-04-23 21:04:01 -0400

    implemented majority of mutant generation logic

--- src/hulk/server/mutant.py
+++ src/hulk/server/mutant.py
@@ -1,26 +1,31 @@
+# TODO mutants are killed when the server is killed
 from typing import List, Iterator, Dict
 from uuid import UUID, uuid4
+import tempfile
 
 from bugzoo.core.bug import Bug
 from bugzoo.client import Client as BugZooClient
 
-from ..core import Mutant, Mutation
+from .sourcefile import SourceFileManager
+from ..config.operators import Operators as OperatorManager
+from ..core import Mutant, Mutation, Replacement
+
+
+# FIXME implement via rooibos.rewrite
+def REWRITER(template: str, arguments: Dict[str, str]) -> str:
+    return template
 
 
 class MutantManager(object):
     def __init__(self,
-                 client_bugzoo: BugZooClient
+                 client_bugzoo: BugZooClient,
+                 operators: OperatorManager,
+                 sources: SourceFileManager
                  ) -> None:
         self.__mutants = [] # type: Dict[UUID, Mutant]
         self.__bugzoo = client_bugzoo
-
-    @property
-    def bugzoo(self) -> BugZooClient:
-        """
-        Provides a connection to a BugZoo server.
-        WARNING: Currently assumed to be running on the same machine.
-        """
-        return self.__bugzoo
+        self.__operators = operators
+        self.__sources = sources
 
     def __iter__(self) -> Iterator[UUID]:
         """
@@ -50,22 +55,74 @@ class MutantManager(object):
     def __len__(self) -> int:
         """
         Returns a count of the number of registered mutants.
-        """
+            """
         return len(self.__mutants)
 
     def generate(self, snapshot: Bug, mutations: List[Mutation]) -> Mutant:
-        # FIXME this is *incredibly* unlikely to conflict
+        bz = self.__bugzoo
+        assert len(mutations) <= 1, "higher-order mutation is currently unsupported"
+
+        # NOTE this is *incredibly* unlikely to conflict
         uuid = uuid4()
         assert uuid not in self.__mutants, "UUID already in use."
 
+        # determine the name of the Docker image for this mutant
+        docker_image = "hulk:{}".format(uuid.hex)
+        mutant_snapshot_name = docker_image
+
+        # group mutations by file
+        file_mutations = [] # type: Dict[str, List[Mutation]]
+        for mutation in mutant.mutations:
+            location = mutation.location
+            filename = location.filename
+
+            if filename not in file_mutations:
+                file_mutations[filename] = []
+            file_mutations[filename].append(mutation)
+
+        # transform each mutation into a replacement and group by file
+        replacements_in_file = {} # type: Dict[str, List[Replacement]]
+        for mutation in mutant.mutations:
+            location = mutation.location
+            filename = location.filename
+            if filename not in replacements_in_file:
+                replacements_in_file[filename] = []
+
+            operator = self.__operators[mutation.operator]
+            transformation = operator.transformations[mutation.transformation_index]
+            text_mutated = REWRITER(transformation.rewrite, mutation.arguments)
+
+            replacement = Replacement(location, text_mutated)
+            replacements_in_file[filename].append(replacement)
+
+        # compute the mutated contents of each affected file
+        mutated_files = {} # type: Dict[str, str]
+        for filename in replacements_in_file:
+            mutated_files[filename] = \
+                self.__sources.apply(snapshot,
+                                     filename,
+                                     replacements_in_file[filename])
+
+        # TODO generate a diff
+
+        # generate the Docker image on the BugZoo server
+        container = bz.containers.provision(snapshot)
+        try:
+            bz.containers.patch(diff)
+            bz.containers.persist(snapshot, docker_image)
+        finally:
+            del bz.containers[container.uid]
+
+        # build snapshot
+        snapshot_mutated = Bug(name=mutant_snapshot_name,
+                               image=docker_image,
+                               languages=snapshot_original.languages,
+                               harness=snapshot_original.harness,
+                               compiler=snapshot_original.compiler,
+                               files_to_instrument=snapshot_original.files_to_instrument)
+
+        # TODO register with BugZoo
+
+        # build and register the mutant
         mutant = Mutant(uuid, snapshot.name, mutations)
-
-        # obtain a diff for the mutant -- save to disk
-
-        # construct files for the mutant
-        # - Dockerfile
-
-        # register with BugZoo
-        # - attempt to build image
-
         return mutant
