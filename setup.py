7b558d8ee558d571ca0fe70b196b05ed608f3600
ChrisTimperley <christimperley@googlemail.com>
2018-04-26 17:11:59 -0400

    setup: added rooibos >= 0.1.0

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     python_requires='>=3.5',
     install_requires=[
         'bugzoo>=2.1.0',
+        'rooibos>=0.1.0',
         'pyyaml',
         'requests>=2.0.0',
         'flask>=0.10',
