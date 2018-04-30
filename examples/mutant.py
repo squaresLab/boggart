b7ddcb16c1a9f973f10e95f72960b50243f95f52
ChrisTimperley <christimperley@googlemail.com>
2018-04-29 20:50:51 -0400

    updated example code

--- examples/mutant.py
+++ examples/mutant.py
@@ -4,55 +4,58 @@ import signal
 import subprocess
 import time
 
-import hulk
+import boggart
 import bugzoo
 
 
 @contextmanager
 def bugzoo_test_server(port: int = 6060) -> None:
     url = "http://127.0.0.1:{}".format(port)
-    cmd = ["bugzood"] # "--port", str(port)]
+    cmd = ["bugzood", "--port", str(port)]
     print("launching bugzoo at {} via '{}'".format(url, ' '.join(cmd)))
     try:
         process = subprocess.Popen(cmd,
-                                   stdout=subprocess.DEVNULL,
-                                   stderr=subprocess.DEVNULL,
+                                   #stdout=subprocess.DEVNULL,
+                                   #stderr=subprocess.DEVNULL,
                                    preexec_fn=os.setsid)
-        # TODO adding waiting to client constructor
-        time.sleep(15)
         yield bugzoo.client.Client(url)
     finally:
         os.killpg(process.pid, signal.SIGTERM)
 
 
 @contextmanager
-def hulk_test_server(url_bugzoo: str, port: int = 8000) -> None:
+def boggart_test_server(url_bugzoo: str, port: int = 8000) -> None:
     url = "http://127.0.0.1:{}".format(port)
-    cmd = ["hulkd", "--bugzoo", url_bugzoo, "-p", str(port)]
-    print("launching hulk at {} via '{}'".format(url, ' '.join(cmd)))
+    cmd = ["boggartd", "--bugzoo", url_bugzoo, "-p", str(port)]
+    print("launching boggartd at {} via '{}'".format(url, ' '.join(cmd)))
     try:
         process = subprocess.Popen(cmd,
-                                   preexec_fn=os.setsid,
-                                   stdout=subprocess.DEVNULL,
-                                   stderr=subprocess.DEVNULL)
-        # TODO adding waiting to client constructor
-        time.sleep(5)
-        yield hulk.Client(url)
+                                   preexec_fn=os.setsid)
+                                   #stdout=subprocess.DEVNULL,
+                                   #stderr=subprocess.DEVNULL)
+        yield boggart.Client(url)
     finally:
         os.killpg(process.pid, signal.SIGTERM)
 
 
 @contextmanager
-def test_env(port_bugzoo: int = 6060, port_hulk: int = 8000) -> None:
+def test_env(port_bugzoo: int = 6060, port_boggart: int = 8000) -> None:
     url_bugzoo = "http://127.0.0.1:{}".format(port_bugzoo)
     with bugzoo_test_server(port_bugzoo) as client_bugzoo:
-        with hulk_test_server(url_bugzoo, port_hulk) as client_hulk:
-            yield (client_bugzoo, client_hulk)
+        with boggart_test_server(url_bugzoo, port_boggart) as client_boggart:
+            yield (client_bugzoo, client_boggart)
 
 
 def generate_mutant():
-    with test_env() as (client_bugzoo, client_hulk):
-        print(list(client_hulk.operators))
+    with test_env() as (client_bugzoo, client_boggart):
+        snapshot = client_bugzoo.bugs["tse2012:gcd"]
+        location = boggart.FileLocationRange.from_string("gcd.c@10:3::10:15")
+        mutation = boggart.Mutation("NEGATE_IF_CONDITION_CSTYLE", 0, location, {})
+        mutations = [mutation]
+        mutant = client_boggart.mutate(snapshot, mutations)
+        print(mutant)
+        print(mutant.uuid)
+        print(mutant.to_dict())
 
 
 if __name__ == '__main__':
